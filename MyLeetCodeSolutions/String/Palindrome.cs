02ce9112770b7f2b04bee783356513692b733399
Huy Xuon Lieu <huyxuonlieu@gmail.com>
2023-10-21 11:26:38 -0700

    added valid palindrome solution

---
+++ MyLeetCodeSolutions/String/Palindrome.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace myLeetCodeSolutions.String;
+
+public class Palindrome
+{
+    public bool IsPalindrome(string s)
+    {
+        StringBuilder Bstr = new StringBuilder();
+        foreach (char c in s.ToCharArray())
+        {
+            if (char.IsLetter(c) || char.IsDigit(c))
+            {
+                Bstr.Append(c);
+            }
+        }
+
+        string str = Bstr.ToString().ToLower();
+        
+        int start = 0;
+        int strLength = str.Length;
+        int end = str.Length - 1;
+
+        while (start <= end)
+        {
+            if (str[start++] != str[end--])
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
