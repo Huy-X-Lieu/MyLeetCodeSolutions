d323f77939a72f3847f8c2255bebd2c7218bbfa5
Huy Xuon Lieu <huyxuonlieu@gmail.com>
2023-10-21 14:32:49 -0700

    added MaximumDepthOfBinaryTree solution

---
+++ MyLeetCodeSolutions/Tree/MaximumDepthOfBinaryTree.cs
@@ -0,0 +1,16 @@
+﻿namespace MyLeetCodeSolutions.Tree;
+
+public class MaximumDepthOfBinaryTree
+{
+    public int MaxDepth(TreeNode root)
+    {
+        if (root == null)
+        {
+            return 0;
+        }
+
+        int leftD = MaxDepth(root.left) + 1;
+        int rightD = MaxDepth(root.right) + 1;
+        return leftD > rightD ? leftD : rightD;
+    }
+}
