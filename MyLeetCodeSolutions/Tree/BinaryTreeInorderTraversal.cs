0ecffd8a1658756051e02336a78fd5b524025869
Huy Xuon Lieu <huyxuonlieu@gmail.com>
2023-10-20 17:08:32 -0700

    Added inorder traversal solution

---
+++ MyLeetCodeSolutions/Tree/BinaryTreeInorderTraversal.cs
@@ -0,0 +1,23 @@
+﻿namespace MyLeetCodeSolutions.Tree;
+
+public class BinaryTreeInorderTraversal
+{
+    public IList<int> InorderTraversal(TreeNode root)
+    {
+        List<int> valList = new List<int>();
+        Helper(root,valList);
+        return valList;
+
+    }
+
+
+    public void Helper(TreeNode root, List<int> list)
+    {
+        if (root != null)
+        {
+            Helper(root.left, list);
+            list.Add(root.val);
+            Helper(root.right, list);
+        }
+    }
+}
