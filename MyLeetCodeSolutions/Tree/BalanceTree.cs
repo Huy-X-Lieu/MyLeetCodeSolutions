4a4746207869d666ee50d69378f2d88779ab013f
Huy Xuon Lieu <huyxuonlieu@gmail.com>
2023-11-08 13:29:19 -0700

    Add solution for Check if a Binary Tree is Balance problem

--- MyLeetCodeSolutions/Tree/BalanceTree.cs
+++ MyLeetCodeSolutions/Tree/BalanceTree.cs
@@ -5,6 +5,34 @@ namespace MyLeetCodeSolutions.Tree;
 
 public class BalanceTree
 {
-    
+    /**
+     * Action: check the height of the left and right subtree of a node to see
+     * if it is balance.
+     * A tree is balanced when it left and right subtrees are balance as well as
+     * their subtrees
+     */
+    public bool IsBalancedTree(TreeNode root)
+    {
+        if (root == null)
+        {
+            return true;
+        }
+        int leftHeight = GetHeight(root.left);
+        int rightHeight = GetHeight(root.right);
+
+        return Math.Abs(leftHeight - rightHeight) < 2 &&
+               IsBalancedTree(root.left) &&
+               IsBalancedTree(root.right);
+    }
+
+    private int GetHeight(TreeNode root)
+    {
+        if (root==null)
+        {
+            return 0;
+        }
+
+        return 1+ Math.Max(GetHeight(root.left), GetHeight(root.right));
+    }
 
 }
