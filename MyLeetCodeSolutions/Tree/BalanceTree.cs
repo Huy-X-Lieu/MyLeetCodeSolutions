9ccab8900a5ed1ba1447f4b7ef9a67bb6ede1e65
Huy Xuon Lieu <huyxuonlieu@gmail.com>
2023-11-08 12:47:37 -0700

    Add the solution for Convert Sorted Array to BST problem.

--- MyLeetCodeSolutions/Tree/BalanceTree.cs
+++ MyLeetCodeSolutions/Tree/BalanceTree.cs
@@ -5,25 +5,6 @@ namespace MyLeetCodeSolutions.Tree;
 
 public class BalanceTree
 {
-    public TreeNode SortedArrayToBST(int[] nums)
-    {
-        TreeNode root = new TreeNode(nums[0]);
-        for (int i = 1; i < nums.Length; i++)
-        {
-            Add(root, nums[i]);
-        }
+    
 
-        return root;
-    }
-
-    public void RightRotate(TreeNode root)
-    {
-        TreeNode tempRoot = root;
-        root = tempRoot.left;
-        if (root.right != null)
-        {
-            tempRoot.left = root.right;
-            root.right = tempRoot;
-        }
-    }
 }
---
+++ MyLeetCodeSolutions/Tree/SortedArrayToBST.cs
@@ -0,0 +1,24 @@
+﻿namespace MyLeetCodeSolutions.Tree;
+
+public class SortedArrayToBST
+{
+    public TreeNode ConvertSortedArrayToBST(int[] nums)
+    {
+        return Helper(nums, 0, nums.Length - 1);
+    }
+
+    private TreeNode Helper(int[] nums, int left, int right)
+    {
+        if (left > right)
+        {
+            return null;
+        }
+
+        int pivot = (right + left) / 2;
+        
+        TreeNode root = new TreeNode(nums[pivot]);
+        root.left     = Helper(nums, left, pivot - 1);
+        root.right = Helper(nums, pivot + 1, right);
+        return root;
+    }
+}
